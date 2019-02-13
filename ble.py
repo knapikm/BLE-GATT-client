0172437afd2d7620c0d1b09c5839058abc3ec447
Michal Knapík <knapikmichal94@gmail.com>
2019-02-13 14:04:33 +0100

    mqtt pato for eth path, new data format into sql table

--- ble.py
+++ ble.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 from bluepy.btle import Scanner, DefaultDelegate, Peripheral, UUID
-import time
+import sys
 import os
 import sqlite3
 from datetime import datetime
-import paho.mqtt.client as mqtt
+import paho.mqtt.client as mqttClient
+import time
+import json
 
 dev = None
 
@@ -13,7 +15,6 @@ def lf_flowerpot():
     global dev
     scanner = Scanner()
     devices = scanner.scan(5.0)
-    finded = False
     for dev in devices:
         if dev.addr == '24:0a:c4:00:61:86':
             try:
@@ -28,13 +29,74 @@ def lf_flowerpot():
 def get_values(val, sql=False):
     #resp = val[0][1].decode('UTF-8')
     id, temp = val[0][1].decode('UTF-8').split(", ")
-    #hum, press = val[1][1].decode('UTF-8').split(", ")
-    hum, press = 0,0
     voltage, moist = val[1][1].decode('UTF-8').split(", ")
-    print(id, temp, hum, press, voltage, moist)
+    print(id, temp, voltage, moist)
     if sql:
-        return (id, temp, hum, press, voltage, moist, 0)
-    return (id, temp, hum, press, voltage, moist)
+        return (id, temp, voltage, moist, 0)
+    return json.dumps( { "id": id, "temperature": temp, "battery": voltage, "moisture": moist, "network": 2 } )
+
+
+
+connected = False  # Stores the connection status
+BROKER_ENDPOINT = "things.ubidots.com"
+PORT = 1883
+MQTT_USERNAME = "A1E-rHXnsEnsjpZKKSlf8khOxgZwnXKkE3"  # Put here your TOKEN
+MQTT_PASSWORD = ""
+TOPIC = "/v1.6/devices/"
+DEVICE_LABEL = "sipy"
+topic = "{}{}".format(TOPIC, DEVICE_LABEL)
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("[MQTT_INFO] Connected to broker")
+        global connected  # Use global variable
+        connected = True  # Signal connection
+
+    else:
+        print("[MQTT_INFO] Error, connection failed")
+
+
+def on_publish(client, userdata, result):
+    print("[MQTT_INFO] Published!")
+
+
+def connect(mqtt_client, mqtt_username, mqtt_password, broker_endpoint, port):
+    global connected
+
+    if not connected:
+        mqtt_client.username_pw_set(mqtt_username, password=mqtt_password)
+        mqtt_client.on_connect = on_connect
+        mqtt_client.on_publish = on_publish
+        mqtt_client.connect(broker_endpoint, port=port)
+        mqtt_client.loop_start()
+
+        attempts = 0
+
+        while not connected and attempts < 5:  # Waits for connection
+            print("[MQTT_INFO] Attempting to connect...")
+            time.sleep(2)
+            attempts += 1
+
+    if not connected:
+        print("[MQTT_ERROR] Could not connect to broker")
+        return False
+
+    return True
+
+
+def publish(mqtt_client, topic, payload):
+    print("[MQTT_INFO] Attempting to publish payload:")
+    print(payload)
+    try:
+        mqtt_client.publish(topic, payload, qos=1)
+    except Exception as e:
+        print("[MQTT_ERROR] There was an error, details: \n{}".format(e))
+
+
+mqtt_client = mqttClient.Client()
+if not connected:  # Connects to the broker
+    connect(mqtt_client, MQTT_USERNAME, MQTT_PASSWORD,
+            BROKER_ENDPOINT, PORT)
 
 time_s = 1
 while True:
@@ -75,12 +137,13 @@ while True:
 
     val = sorted(val, key=lambda tup: tup[0].binVal)
     if os.system('ping -q -I eth0 -w 1 -c 1 8.8.8.8 > /dev/null') == 0:
-        get_values(val)
+        publish(mqtt_client, topic, payload=get_values(val))
+        time.sleep(2)
     else:
         try:
             conn = sqlite3.connect('flower_pot_data.db', isolation_level=None)
             c = conn.cursor()
-            sql = ''' INSERT INTO measurements(id, temperature, humidity, pressure, battery, moisture, global) VALUES (?,?,?,?,?,?,?) '''
+            sql = ''' INSERT INTO measurements(id, temperature, battery, moisture, global) VALUES (?,?,?,?,?) '''
             c.execute(sql, get_values(val, sql=True))
             conn.close()
             print('SQL done, last row id is:', c.lastrowid)
